0e5ab694c9507e5cddfc76073f014383bf6b6c46
DevAdedeji <adedejitewogbade2@gmail.com>
2022-07-17 19:34:24 +0100

    Learnt about beforeEach

---
+++ setup/setup.test.js
@@ -0,0 +1,23 @@
+let animals = ['elephant', 'zebra', 'bear', 'tiger']
+
+beforeEach(()=>{
+    console.log("BEFORE EACH TEST");
+    animals = ['elephant', 'zebra', 'bear', 'tiger']
+})
+
+describe("animals array", ()=>{
+    it("Should add animal to end of array", ()=>{
+        
+        animals.push("alligator")
+        expect(animals[animals.length - 1]).toBe("alligator")
+    })
+
+    it("Should add animal to beginning of array", ()=>{
+        animals.unshift("monkey")
+        expect(animals[0]).toBe("monkey")
+    })
+
+    it("should have initial length of 4", ()=>{
+        expect(animals.length).toBe(4)
+    })
+})
