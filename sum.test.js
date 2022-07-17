a6c4a94bdf870717716fca9b733f9471b5bd12d7
DevAdedeji <adedejitewogbade2@gmail.com>
2022-07-17 18:45:55 +0100

    Writing test on numbers

--- sum.test.js
+++ sum.test.js
@@ -1,16 +1,37 @@
 const sum = require("./sum")
 
-describe("example tests", ()=>{
-    it("should add 1 + 2 to equal 3", ()=>{
-        const result =sum(1,2)
-        expect(result).toBe(3)
-    })
-    it("object assignment",()=>{
-        const obj = {};
-        expect(obj).toEqual({})
-    })
-})
+// describe("example tests", ()=>{
+//     it("should add 1 + 2 to equal 3", ()=>{
+//         const result =sum(1,2)
+//         expect(result).toBe(3)
+//     })
+//     it("object assignment",()=>{
+//         const obj = {};
+//         expect(obj).toEqual({})
+//     })
+// })
 
-describe("Truthy or false", ()=>{
-    
-})
+// describe("Truthy or false", ()=>{
+//     it("null", ()=>{
+//         const n = null;
+//         // expect(n).toBeFalsy()
+//         // expect(n).not.toBeTruthy() 
+//         // expect(n).toBeNull()
+//         // expect(n).toBeUndefined()
+//     })
+// })
+
+// describe("numbers", ()=>{
+//     it("two plus two", ()=>{
+//         const value = 2 + 2
+//         // expect(value).toBe(4)
+//         // expect(value).toBeGreaterThan(4)
+//         // expect(value).toBeGreaterThanOrEqual(3)
+//         // expect(value).toBeLessThan(5)
+//         // expect(value).toBeLessThanOrEqual(4)
+//     })
+//     it("adding floats", ()=>{
+//         const value = 0.1 + 0.2;
+//         expect(value).toBeCloseTo(0.29)
+//     })
+// })
