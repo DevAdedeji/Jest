3d641dccfd8f7716b2f82a713c1cd15cc54a302f
DevAdedeji <adedejitewogbade2@gmail.com>
2022-07-17 18:31:01 +0100

    Learnt about describe which can be used to group different test case

--- sum.test.js
+++ sum.test.js
@@ -1,6 +1,16 @@
 const sum = require("./sum")
 
-it("should add 1 + 2 to equal 3", ()=>{
-    const result = sum(1,2);
-    expect(result).toBe(3)
+describe("example tests", ()=>{
+    it("should add 1 + 2 to equal 3", ()=>{
+        const result =sum(1,2)
+        expect(result).toBe(3)
+    })
+    it("object assignment",()=>{
+        const obj = {};
+        expect(obj).toEqual({})
+    })
+})
+
+describe("Truthy or false", ()=>{
+    
 })
